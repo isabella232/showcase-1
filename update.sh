a78b6cdb5dc3af39065c7dddd9c1db197f8b3a91
Christian Grigis <christian.grigis@epfl.ch>
2020-03-20 13:02:12 +0100

    Recreate the Python virtual env using the docker image

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+APACHE_DOCKER_IMAGE="c4dt/apache:latest"
+# Must match the path used when running the actual Apache service
+SHOWCASE_MOUNTPOINT="/var/www/showcase"
+
+cd ~/showcase
+
+# Clean local changes (if any)
+git checkout .
+
+git pull
+
+# Recreate virtual env
+docker run \
+    --rm \
+    --mount type=bind,src=/home/showcase/showcase,dst=${SHOWCASE_MOUNTPOINT} \
+    --user `id -u`:`id -g` \
+    ${APACHE_DOCKER_IMAGE} \
+    make -C ${SHOWCASE_MOUNTPOINT} clean env
+
+# Touching the main WSGI app file triggers a reload
+touch app.wsgi
